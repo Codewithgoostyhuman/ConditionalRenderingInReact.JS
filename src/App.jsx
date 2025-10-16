7797273ad8103cfc3ee0e9103da7900d14e901b9
Codewithgoostyhuman <zeeshantahiralvi123@gmail.com>
2025-10-17 00:02:32 +0500

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { createContext, useContext, useState } from 'react'
-import Child from './components/Child';
+
 
 
 function App() {
-  
+ const [Email,setEmail] = useState("");
+ function handleSubmit(e){
+  e.preventDefault();
+  alert(`Form Submitted - Email: ${Email}`);
+ }
   
 return(
+<div>
+  <form onSubmit={handleSubmit}>
+    <input type="email" value={Email} placeholder='Enter your email' onChange={(e)=> setEmail(e.target.value)}/>
+    <button type='submit'>Submit</button>
+  </form>
 
+</div>
   
   
 
